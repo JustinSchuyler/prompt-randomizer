6569a7bb08bb4a59a14a9c84bcc3beeaba7ee71b
Justin Schuyler <justin.r.schuyler@gmail.com>
2020-08-23 17:54:12 -0600

    init(repo)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ConfettiGenerator from 'confetti-js';
 import './App.css';
 
+// EDIT THESE
+const prompts = [
+  'prompt 1',
+  'prompt 2',
+  'prompt 3',
+  'prompt 4',
+  'prompt 5'
+];
+
+
+
+// 0 inclusive to max not inclusive
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function App() {
+  const [current, setCurrent] = useState('');
+  const [firstTime, setFirstTime] = useState(true);
+  const title = 'Randomized Prompts!'
+
+  const handleRandomPrompt = () => {
+    if (prompts.length === 0) {
+      setCurrent('No more prompts!');
+      return;
+    }
+    if (firstTime) {
+      const confettiSettings = { target: 'canvas' };
+      const confetti = new ConfettiGenerator(confettiSettings);
+      confetti.render();
+      setFirstTime(false);
+
+      setTimeout(() => {
+        const rand = getRandomInt(prompts.length);
+        setCurrent(prompts.splice(rand, 1));
+      }, 2000);
+    } else {
+      const rand = getRandomInt(prompts.length);
+      setCurrent(prompts.splice(rand, 1));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="canvas"></canvas>
+      <div className="category-selector">
+        <h1>{(!current) ? title : current}</h1>
+        <button type="button" onClick={handleRandomPrompt}>New Prompt!</button>
+      </div>
     </div>
   );
 }
